8e75020f66255502baea704fb37b8c229de31f1e
Noman Rajput <nomanrajput784@gmail.com>
2020-11-16 12:36:48 +0500

    new changes

--- readlineInput.js
+++ readlineInput.js
@@ -2,12 +2,9 @@ var readline = require('readline');
 var takeInput = readline.createInterface(process.stdin, process.stdout);
 
 takeInput.question("Enter first value??? ", function(x){
-    takeInput.question("Enter second value", function(y){
-
-        var count = parseInt(x) + parseInt(y); 
-
-        console.log("the addition of x + y = " +count);
+    
+        console.log("you entered "+ x)
 
         takeInput.close();
-    })
+ 
 });
